f730ea6b81cdaef51b9b92221fd4dfd16e9f6da5
Max <MaksimK_dev@044.agency>
2026-08-20 15:59:09 +0300

    update branch01

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,3 @@ rootProject.name = "app"
 
 include("branch01_basic:notification-service")
 include("branch01_basic:order-service")
-
-include("branch02_topics_partitions:notification-service")
-include("branch02_topics_partitions:order-service")
